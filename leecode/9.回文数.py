672edc912c4dad11f7b64194432a6ba82a59ee0b
liuliangdong <liuliangdong@qiyandata.com>
2020-04-23 19:06:32 +0800

    updated one

--- leecode/9.回文数.py
+++ leecode/9.回文数.py
@@ -8,10 +8,18 @@
 class Solution:
     """"""
     def isPalindrome(self, x: int) -> bool:
-        if x < 0:
+        if x < 0 or (x % 10 == 0 and x != 0):
             return False
         right = 0
+
         while x > right:
-            
+            print(x, right)
+            right = right * 10 + x % 10
+            x //= 10
+        print(x, right)
+        return x == right or x == right // 10
+
+
+print(Solution().isPalindrome(0))
 # @lc code=end
 
